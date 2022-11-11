b6e7573cf970c5b5db17cf5a3703197c5468aaec
koki-fore <furukunn@gmail.com>
2022-11-12 01:21:19 +0900

    challenges_completedが完成

--- app/routers/challenges_completed.py
+++ app/routers/challenges_completed.py
@@ -1,20 +1,20 @@
+from typing import List
+
 from fastapi import APIRouter
+
+import app.schemas.challenges_completed as challenges_completed_schema
 
 router = APIRouter()
 
 
-@router.get("/challenge-completed")
+@router.get("/challenge-completed", response_model=List[challenges_completed_schema.Challenges_completed])
 async def list_challenge_completed():
     pass
 
-@router.post("/challenge-completed")
-async def create_challenge_completed():
-    pass
+@router.post("/challenge-completed", response_model=challenges_completed_schema.Challenges_completed)
+async def create_challenge_completed(challenges_completed_body: challenges_completed_schema.Challenges_completed):
+    return challenges_completed_schema.Challenges_completed(**challenges_completed_body.dict())
 
-@router.put("/challenge-completed/{challenge_completed_id}")
-async def update_challenge_completed():
-    pass
-
-@router.delete("/challenge-completed/{challenge_completed_id}")
-async def delete_challenge_completed():
-    pass
+@router.delete("/challenge-completed/{challenges_completed_id}", response_model=None)
+async def delete_challenge_completed(challenges_completed_id: int):
+    return
