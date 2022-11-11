9b52b6b4a0d34c9ae6550cb91a6b0ccdaa8993a5
koki-fore <furukunn@gmail.com>
2022-11-11 22:23:31 +0900

    postsのschemasを完成

--- app/schemas/posts.py
+++ app/schemas/posts.py
@@ -5,7 +5,6 @@ from pydantic import BaseModel, Field
 # APIのリクエストやレスポンスの型を定義する
 
 class PostBase(BaseModel):
-    id: int
     user_FK: int
     challenge_FK: Optional[int] = Field(None)
     text: str = Field(None, example='貯蓄用の水を買いました')
@@ -13,7 +12,6 @@ class PostBase(BaseModel):
     picture_path_02: Optional[str] = Field(None)
     picture_path_03: Optional[str] = Field(None)
     picture_path_04: Optional[str] = Field(None)
-    like_count: Optional[int] = Field(0, example=100)
     created_at: datetime
     updated_at: datetime
     
@@ -21,13 +19,15 @@ class PostCreate(PostBase):
     pass
         
 class PostCreateResponse(PostCreate):
-    pass
+    id: int
+    like_count: int
     
     class Config:
         orm_mode = True
 
 class Post(PostBase):
-    pass
+    id: int
+    like_count: int = Field(0, example=100)
     
     class Config:
         orm_mode = True
