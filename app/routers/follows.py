f29ca2f14e50a5259825740f6e2c3ad5a45f9462
koki-fore <furukunn@gmail.com>
2022-11-13 04:07:15 +0900

    follow completed

--- app/routers/follows.py
+++ app/routers/follows.py
@@ -1,20 +1,20 @@
+from typing import List
+
 from fastapi import APIRouter
+
+import app.schemas.follow as follow_schema
 
 router = APIRouter()
 
 
-@router.get("/fallows")
+@router.get("/fallows", response_model=List[follow_schema.Follow])
 async def list_fallows():
     pass
 
-@router.post("/fallows")
-async def create_fallow():
-    pass
+@router.post("/fallows", response_model=follow_schema.Follow)
+async def create_fallow(follow_body: follow_schema.Follow):
+    return follow_schema.Follow(**follow_body.dict())
 
-@router.put("/fallows/{fallow_id}")
-async def update_fallow():
-    pass
-
-@router.delete("/fallows/{fallow_id}")
+@router.delete("/fallows/{fallow_id}", response_model=None)
 async def delete_fallow():
-    pass
+    return
