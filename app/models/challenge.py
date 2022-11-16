81f764aaedc65f9d605393be40b938613b543cb6
koki-fore <furukunn@gmail.com>
2022-11-16 10:20:45 +0900

    completed challenge model

---
+++ app/models/challenge.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, TIMESTAMP, text
+from sqlalchemy.orm import relationship
+
+from .base import TimestampMixin
+from app.db import Base
+
+
+class Challenge_category(Base, TimestampMixin):
+    __tablename__ = "challenges"
+    
+    id = Column(Integer, primary_key=True)
+    title = Column(String(1024), nullable=False)
+    text = Column(String(1024))
+    experience_point = Column(Integer, default=0, nullable=False)
