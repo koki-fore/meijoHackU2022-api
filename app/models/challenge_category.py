d3c0d1a2743f19f9fb2daaff04712690b47639cb
koki-fore <furukunn@gmail.com>
2022-11-16 10:10:34 +0900

    complete challenge_category model

---
+++ app/models/challenge_category.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, TIMESTAMP, text
+from sqlalchemy.orm import relationship
+
+from .base import TimestampMixin
+from app.db import Base
+
+
+class Challenge_category(Base, TimestampMixin):
+    __tablename__ = "challenge_categories"
+    
+    id = Column(Integer, primary_key=True)
+    challenge_FK = Column(Integer, ForeignKey('challenges.id'))
+    category_FK = Column(Integer, ForeignKey('categories.id'))
